10763859589234ce2e4ed79c09aae8fee7d3afbc
raimokoi <raimo.koidam@ut.ee>
2024-05-17 16:09:46 +0300

    Fixed a typo

--- demo_lighting/demo_lighting.py
+++ demo_lighting/demo_lighting.py
@@ -5,32 +5,32 @@ import os
 
 node = None
 pub_mode = None
-mode = 0
+mode = 1
 
 def change_mode():
-    global mode
+    global mode, pub_mode, node
     led_msg_mode = LedModuleMode()
     led_msg_mode.mode = mode
     if mode == 1 or mode == 2:
-        led_msg_mode.prams.append(random.randint(0, 255))
-        led_msg_mode.prams.append(random.randint(0, 255))
-        led_msg_mode.prams.append(random.randint(0, 255))
-        led_msg_mode.prams.append(random.randint(1, 50))
+        led_msg_mode.params.append(random.randint(0, 255))
+        led_msg_mode.params.append(random.randint(0, 255))
+        led_msg_mode.params.append(random.randint(0, 255))
+        led_msg_mode.params.append(random.randint(1, 50))
     if mode == 3 or mode == 4 or mode == 6:
-        led_msg_mode.prams.append(random.randint(1, 50))
+        led_msg_mode.params.append(random.randint(1, 50))
     
     pub_mode.publish(led_msg_mode)
-    #node.get_logger().info('Publishing: "%s"' % led_msg_mode)
+    node.get_logger().info('Publishing: "%s"' % led_msg_mode)
     mode += 1
     if mode >= 9:
-        mode = 0
+        mode = 1
 
 def main(args=None):
     global pub_mode, node
     rclpy.init(args=args)
     node = rclpy.create_node('demo_lighting')
     pub_mode = node.create_publisher(LedModuleMode, 'led_mode', 10)
-    node.create_timer(10, change_mode)
+    node.create_timer(3, change_mode)
     try:
         rclpy.spin(node)
     except:
